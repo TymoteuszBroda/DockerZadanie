094dafb48b4191e13e90d75d80abdc8990b3b713
Tymek <Tymoteusz.91100@gmail.com>
2024-02-08 11:21:08 +0100

    added get, post put and delete functionality

--- file.py
+++ file.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import List
 
 class Database:
     elves = {}
@@ -14,3 +15,26 @@ class Package(BaseModel):
     assigned_to: str = None
 
 app = FastAPI()
+
+@app.get("/elves/", response_model=List[Elf])
+async def get_elves():
+    return Database.elves.values()
+
+@app.post("/elves/")
+async def create_elf(elf: Elf):
+    Database.elves[elf.name] = elf
+    return elf
+
+@app.put("/elves/{elf_name}")
+async def update_elf(elf_name: str, elf: Elf):
+    if elf_name not in Database.elves:
+        raise HTTPException(status_code=404, detail="Elf not found")
+    Database.elves[elf_name] = elf
+    return elf
+
+@app.delete("/elves/{elf_name}")
+async def delete_elf(elf_name: str):
+    if elf_name not in Database.elves:
+        raise HTTPException(status_code=404, detail="Elf not found")
+    del Database.elves[elf_name]
+    return {"message": "Elf usunięty"}
