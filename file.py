dc30bec0f48acc8a563dbf5af70471fe29d79df3
Tymek <Tymoteusz.91100@gmail.com>
2024-02-08 11:19:13 +0100

    added dummy database and basic classes

--- file.py
+++ file.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+class Database:
+    elves = {}
+    packages = {}
+
+class Elf(BaseModel):
+    name: str
+    is_available: bool = True
+
+class Package(BaseModel):
+    content: str
+    assigned_to: str = None
 
 app = FastAPI()
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
