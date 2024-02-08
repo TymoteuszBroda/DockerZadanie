fc6bae6c3c2e45fb379b19b593aa5279501383bb
Tymek <Tymoteusz.91100@gmail.com>
2024-02-08 11:51:24 +0100

    DB code added

--- db.py
+++ db.py
@@ -2,3 +2,21 @@ import databases
 import sqlalchemy
 
 DATABASE_URL = "postgresql://user:password@localhost/packagemanagement"
+
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+packages = sqlalchemy.Table(
+    "packages",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("content", sqlalchemy.String),
+    sqlalchemy.Column("assigned_to", sqlalchemy.String),
+)
+
+elves = sqlalchemy.Table(
+    "elves",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String),
+    sqlalchemy.Column("is_available", sqlalchemy.Boolean),
+)
